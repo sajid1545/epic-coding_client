6c7a298d91795d5be545bb85752253d2e5ee5e51
sajid9962 <clashking1545@gmail.com>
2022-10-24 22:51:22 +0600

    dark/light mode toogle icons added

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,113 +1,89 @@
 import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import logo from '../../assets/logo.jpg';
+import { FaBars, FaCrosshairs, FaMoon, FaSun, FaTimes } from 'react-icons/fa';
 
 const Header = () => {
 	const [isMenuOpen, setIsMenuOpen] = useState(false);
+	const [darkMode, setDarkMode] = useState(true);
+
+	function toggleDarkMode() {
+		setDarkMode((prevDarkMode) => !prevDarkMode);
+	}
 
 	return (
-		<div>
-			<div class="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
-				<div class="relative flex items-center justify-between">
-					<a href="/" aria-label="Company" title="Company" class="inline-flex items-center">
-                        <img src={ logo } alt="" className='w-10 h-10 rounded-full' />
-						<span class="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
+		<div className="">
+			<div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+				<div className="relative flex items-center justify-between">
+					<a href="/" aria-label="Company" title="Company" className="inline-flex items-center">
+						<img src={logo} alt="" className="w-10 h-10 rounded-full" />
+						<span className="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
 							Epic! Coding
 						</span>
 					</a>
-					<ul class=" items-center hidden space-x-8 lg:flex">
+					<ul className=" items-center hidden space-x-8 lg:flex">
 						<li>
-							<a
-								href="/"
-								aria-label="Our product"
-								title="Our product"
-								class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">
-								Product
-							</a>
+							<NavLink
+								to=""
+								title="Courses"
+								className={`font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400 ${(
+									isActive
+								) => (isActive ? 'text-red-700' : undefined)}
+								`}>
+								Courses
+							</NavLink>
 						</li>
 						<li>
-							<a
-								href="/"
-								aria-label="Our product"
-								title="Our product"
-								class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">
-								Features
-							</a>
+							<NavLink
+								to=""
+								title="FAQ"
+								className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">
+								FAQ
+							</NavLink>
 						</li>
 						<li>
-							<a
-								href="/"
-								aria-label="Product pricing"
-								title="Product pricing"
-								class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">
-								Pricing
-							</a>
+							<NavLink
+								to=""
+								title="Blog"
+								className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">
+								Blog
+							</NavLink>
 						</li>
-						<li>
-							<a
-								href="/"
-								aria-label="About us"
-								title="About us"
-								class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">
-								About us
-							</a>
+
+						<li onClick={toggleDarkMode} className="cursor-pointer text-xl">
+							{darkMode ? <FaSun /> : <FaMoon />}
 						</li>
+
 						<li>
-							<a
-								href="/"
-								class="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
-								aria-label="Sign up"
-								title="Sign up">
-								Sign up
-							</a>
+							<button
+								className="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide  transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+								aria-label="Login"
+								title="Login">
+								Sign In
+							</button>
 						</li>
 					</ul>
-					<div class="lg:hidden">
+					<div className="lg:hidden">
 						<button
 							aria-label="Open Menu"
 							title="Open Menu"
-							class="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline hover:bg-deep-purple-50 focus:bg-deep-purple-50"
+							className="p-2 -mr-1 transition duration-200 rounded focus:outline-none focus:shadow-outline hover:bg-deep-purple-50 focus:bg-deep-purple-50"
 							onClick={() => setIsMenuOpen(true)}>
-							<svg class="w-5 text-gray-600" viewBox="0 0 24 24">
-								<path
-									fill="currentColor"
-									d="M23,13H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,13,23,13z"
-								/>
-								<path
-									fill="currentColor"
-									d="M23,6H1C0.4,6,0,5.6,0,5s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,6,23,6z"
-								/>
-								<path
-									fill="currentColor"
-									d="M23,20H1c-0.6,0-1-0.4-1-1s0.4-1,1-1h22c0.6,0,1,0.4,1,1S23.6,20,23,20z"
-								/>
-							</svg>
+							<FaBars className="w-7 h-6 text-gray-600" />
 						</button>
 						{isMenuOpen && (
-							<div class="absolute top-0 left-0 w-full">
-								<div class="p-5 bg-white border rounded shadow-sm">
-									<div class="flex items-center justify-between mb-4">
+							<div className="absolute top-0 left-0 w-full">
+								<div className="p-5 bg-white border rounded shadow-sm">
+									<div className="flex items-center justify-between mb-4">
 										<div>
 											<a
 												href="/"
 												aria-label="Company"
 												title="Company"
-												class="inline-flex items-center">
-												<svg
-													class="w-8 text-deep-purple-accent-400"
-													viewBox="0 0 24 24"
-													strokeLinejoin="round"
-													strokeWidth="2"
-													strokeLinecap="round"
-													strokeMiterlimit="10"
-													stroke="currentColor"
-													fill="none">
-													<rect x="3" y="1" width="7" height="12" />
-													<rect x="3" y="17" width="7" height="6" />
-													<rect x="14" y="1" width="7" height="6" />
-													<rect x="14" y="11" width="7" height="12" />
-												</svg>
-												<span class="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
-													Company
+												className="inline-flex items-center">
+												<img src={logo} alt="" className="w-10 h-10 rounded-full" />
+												<span className="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
+													Epic! Coding
 												</span>
 											</a>
 										</div>
@@ -115,25 +91,20 @@ const Header = () => {
 											<button
 												aria-label="Close Menu"
 												title="Close Menu"
-												class="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+												className="p-2 -mt-2 -mr-2 transition duration-200 rounded hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
 												onClick={() => setIsMenuOpen(false)}>
-												<svg class="w-5 text-gray-600" viewBox="0 0 24 24">
-													<path
-														fill="currentColor"
-														d="M19.7,4.3c-0.4-0.4-1-0.4-1.4,0L12,10.6L5.7,4.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l6.3,6.3l-6.3,6.3 c-0.4,0.4-0.4,1,0,1.4C4.5,19.9,4.7,20,5,20s0.5-0.1,0.7-0.3l6.3-6.3l6.3,6.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3 c0.4-0.4,0.4-1,0-1.4L13.4,12l6.3-6.3C20.1,5.3,20.1,4.7,19.7,4.3z"
-													/>
-												</svg>
+												<FaTimes className="w-6 h-6 text-gray-600" />
 											</button>
 										</div>
 									</div>
 									<nav>
-										<ul class="space-y-4">
+										<ul className="space-y-4">
 											<li>
 												<a
 													href="/"
 													aria-label="Our product"
 													title="Our product"
-													class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">
+													className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">
 													Product
 												</a>
 											</li>
@@ -142,7 +113,7 @@ const Header = () => {
 													href="/"
 													aria-label="Our product"
 													title="Our product"
-													class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">
+													className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">
 													Features
 												</a>
 											</li>
@@ -151,7 +122,7 @@ const Header = () => {
 													href="/"
 													aria-label="Product pricing"
 													title="Product pricing"
-													class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">
+													className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">
 													Pricing
 												</a>
 											</li>
@@ -160,14 +131,14 @@ const Header = () => {
 													href="/"
 													aria-label="About us"
 													title="About us"
-													class="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">
+													className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400">
 													About us
 												</a>
 											</li>
 											<li>
 												<a
 													href="/"
-													class="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+													className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
 													aria-label="Sign up"
 													title="Sign up">
 													Sign up
